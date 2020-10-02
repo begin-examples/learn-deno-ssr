b373a107a7a87577719cbbcd89910adf806f3838
kj <kristoferjoseph@gmail.com>
2020-10-02 09:58:18 -0700

    Updates test to use breaking change to assertStrictEquals.

--- test/index_test.ts
+++ test/index_test.ts
@@ -1,7 +1,7 @@
-import { assertStrictEq } from "https://deno.land/std/testing/asserts.ts";
+import { assertStrictEquals } from "https://deno.land/std/testing/asserts.ts";
 
-Deno.test("isStrictlyEqual", function (): void {
+Deno.test("isStrictlyEquals", function (): void {
   const a = {};
   const b = a;
-  assertStrictEq(a, b);
+  assertStrictEquals(a, b);
 });
